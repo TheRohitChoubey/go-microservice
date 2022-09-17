ec58a6cc8c84e1a36f3f1a723d3e3238f460ae35
Rohit Choubey <rohitc.choubey@gmail.com>
2022-09-17 17:13:32 +0530

    adding basic changes to web ms

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/TheRohitChoubey/go-microservice.git
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Checking Application Health")
+	response := map[string]string{
+		"status":    "UP",
+		"timestamp": time.Now().String(),
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving HomePage")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Application is Up and Running")
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website!")
-	})
-
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.ListenAndServe(":8082", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", rootHandler)
+	r.HandleFunc("/health", healthHandler)
+	log.Println("Server started at :80")
+	log.Fatal(http.ListenAndServe(":80", r))
 }
